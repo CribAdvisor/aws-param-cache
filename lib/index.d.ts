a7b66173afee43657e0cc72287502421a0dad44b
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2020-03-28 19:15:20 +0000

    Add JSDoc to types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,12 @@
 interface SSMCacheOptions {
+    /**
+     * Sets the parameter type: `true` to use `SecureString` `false` to use `String`
+     */
     secret?: Boolean,
+    /**
+     * Where the parameters are stored within SSM *(excluding trailing slash)*
+     * Be sure to update the IAM policy (see `README.md`) if changed
+     */
     basePath?: String
 }
 
@@ -8,7 +15,17 @@ export = SSMCache;
 declare class SSMCache {
     constructor(options: SSMCacheOptions);
 
+    /**
+     * Get a parameter from SSM with the given key (excluding `basePath`)
+     * @param key Name of the SSM parameter (within `basePath`)
+     */
     get(key: String): Promise<String> | null;
 
+    /**
+     * `PUT` a parameter to SSM for the given key (excluding `basePath`)
+     * @param key Name of the SSM parameter to store (within `basePath`)
+     * @param value Value to store in SSM for the key
+     * @param ttl How long in seconds after setting the parameter it should expire
+     */
     set(key: String, value: String, ttl: Number): Promise<PromiseResult<AWS.SSM.PutParameterResult, AWS.AWSError>>;
 }
